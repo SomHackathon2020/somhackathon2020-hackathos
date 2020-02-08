79378e41b9ae8f6b666ac3e514543ba6998bc586
David Molina <david.molina@buypower.es>
2020-02-08 16:25:36 +0100

    Saving Changes

--- src/app/questionario/questionario.component.ts
+++ src/app/questionario/questionario.component.ts
@@ -7,9 +7,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionarioComponent implements OnInit {
 
+  public categories:  any[];
+
   constructor() { }
 
   ngOnInit() {
+    this.categories = [{
+      "name": "Deportes",
+      "subcategories": [
+        {name: "Futbol", value: "football"},
+        {name: "Natació", value: "swimming"},
+        {name: "Esports Electrònics", value: "esports"},
+        {name: "Basquet", value: "basketball"},
+        {name: "Handbol", value: "handball"},
+        {name: "Altres", value: "others"}
+      ]
+    },
+    {
+      "name": "Deportes",
+      "subcategories": [
+        {name: "Futbol", value: "football"},
+        {name: "Natació", value: "swimming"},
+        {name: "Esports Electrònics", value: "esports"},
+        {name: "Basquet", value: "basketball"},
+        {name: "Handbol", value: "handball"},
+        {name: "Altres", value: "others"}
+      ]
+    },
+    {
+      "name": "Deportes",
+      "subcategories": [
+        {name: "Futbol", value: "football"},
+        {name: "Natació", value: "swimming"},
+        {name: "Esports Electrònics", value: "esports"},
+        {name: "Basquet", value: "basketball"},
+        {name: "Handbol", value: "handball"},
+        {name: "Altres", value: "others"}
+      ]
+    },
+    {
+      "name": "Deportes",
+      "subcategories": [
+        {name: "Futbol", value: "football"},
+        {name: "Natació", value: "swimming"},
+        {name: "Esports Electrònics", value: "esports"},
+        {name: "Basquet", value: "basketball"},
+        {name: "Handbol", value: "handball"},
+        {name: "Altres", value: "others"}
+      ]
+    },
+    {
+      "name": "Deportes",
+      "subcategories": [
+        {name: "Futbol", value: "football"},
+        {name: "Natació", value: "swimming"},
+        {name: "Esports Electrònics", value: "esports"},
+        {name: "Basquet", value: "basketball"},
+        {name: "Handbol", value: "handball"},
+        {name: "Altres", value: "others"}
+      ]
+    }];
   }
 
 }
